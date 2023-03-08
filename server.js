915cc04123eccd4ef8df1ccdff02520c9182d98b
Yaswanth Sirigiri <93373615+yaswanths2002@users.noreply.github.com>
2023-03-08 21:29:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Creating Our First Server
-const http = require("http");
+const http = require("https");
 const port = 8081;
-http.createServer((req,res) => {
+https.createServer((req,res) => {
     res.writeHead(200,{"content-Type":"text/html"});
     res.write("<h4>Hello World!!This is my First Server </h4>");
     res.end();
